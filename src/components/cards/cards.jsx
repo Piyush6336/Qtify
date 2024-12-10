544ae8e7d653b83a55e8618b73a6a125e7aee5bc
Piyush arya <piyush.arya@letzcreate.in>
2024-12-10 16:16:09 +0530

    Qtify

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { Card, CardMedia, CardContent, Typography, Chip } from "@mui/material";
 import PropTypes from "prop-types";
-import './cards.css';
+
 const AlbumCard = ({ image, title, follows, category }) => {
   return (
-    <Card style={{ maxWidth: 200, margin: 10, borderRadius: 10 }}>
+    <Card
+      data-testid="album-card"
+      style={{ maxWidth: 200, margin: 10, borderRadius: 10 }}
+    >
       <CardMedia
         component="img"
         height="140"
         image={image}
         alt={title}
         style={{ objectFit: "cover" }}
+        data-testid="album-image"
       />
       <CardContent>
-        <Chip label={`${follows} Follows`} style={{ marginBottom: 8 }} />
-        <Typography variant="subtitle1" gutterBottom>
+        <Chip
+          label={`${follows} Follows`}
+          style={{ marginBottom: 8 }}
+          data-testid="album-follows"
+        />
+        <Typography
+          variant="subtitle1"
+          gutterBottom
+          data-testid="album-title"
+        >
           {title}
         </Typography>
-        <Typography variant="body2" color="textSecondary">
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          data-testid="album-category"
+        >
           {category}
         </Typography>
       </CardContent>

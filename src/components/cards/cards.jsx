9322101fc5d5af077164e785a55f54ecb7d0f4d9
Piyush arya <piyush.arya@letzcreate.in>
2024-12-10 16:54:29 +0530

    Qtify

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,51 +1,57 @@
-import React from "react";
-import { Card, CardMedia, CardContent, Typography, Chip } from "@mui/material";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import PropTypes from "prop-types";
-
-const AlbumCard = ({ image, title, follows, category }) => {
+const MediaCard = ({ album }) => {
+    MediaCard.propTypes = {
+        album: PropTypes.shape({
+          image: PropTypes.string.isRequired,
+          title: PropTypes.string.isRequired,
+          follows: PropTypes.number.isRequired,
+        }).isRequired,
+      };
   return (
     <Card
-      data-testid="album-card"
-      style={{ maxWidth: 200, margin: 10, borderRadius: 10 }}
+      sx={{
+        width: 159,
+        height: 205,
+        gap: 0,
+        borderRadius: "10px 10px 0 0",
+        overflow: "hidden",
+      }}
     >
       <CardMedia
-        component="img"
-        height="140"
-        image={image}
-        alt={title}
-        style={{ objectFit: "cover" }}
-        data-testid="album-image"
+        sx={{ height: "60%" }} // Adjust the height to maintain aspect ratio
+        image={album.image} // Use album's image URL
+        title={album.title} // Use album's title for accessibility
+        alt={`${album.title} album cover`}
       />
-      <CardContent>
-        <Chip
-          label={`${follows} Follows`}
-          style={{ marginBottom: 8 }}
-          data-testid="album-follows"
-        />
-        <Typography
-          variant="subtitle1"
-          gutterBottom
-          data-testid="album-title"
-        >
-          {title}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="textSecondary"
-          data-testid="album-category"
-        >
-          {category}
-        </Typography>
-      </CardContent>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ padding: "8px", justifyContent: "flex-start" }}
+      >
+        <Chip label={`${album.follows} Follows`} color="primary" variant="outlined" />
+
+      </Stack>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{
+          paddingLeft: "8px",
+          textAlign: "left",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+      >
+        {album.title} {/* Display the album title */}
+      </Typography>
     </Card>
   );
 };
 
-AlbumCard.propTypes = {
-  image: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  follows: PropTypes.number.isRequired,
-  category: PropTypes.string,
-};
-
-export default AlbumCard;
+export default MediaCard;
